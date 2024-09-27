4915b9ec60c9f5214b4b79fa37c9d96b9a9637b5
Mbishibishi Leon <mbishibishileon@Mbishibishis-MacBook-Air.local>
2024-09-27 15:22:26 +0300

    REST commit

--- src/main/java/fi/haagahelia/bookstore/domain/BookRepository.java
+++ src/main/java/fi/haagahelia/bookstore/domain/BookRepository.java
@@ -1,8 +1,12 @@
 package fi.haagahelia.bookstore.domain;
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import java.util.List;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
 }
